49ca5ff6416b1669b162e33dfd1c88addf791ae5
khyunwook <khyunwook0707@gmail.com>
2024-11-13 11:34:50 +0900

    chore: 조금 수정

--- src/class/Note.svelte.ts
+++ src/class/Note.svelte.ts
@@ -1,4 +1,4 @@
-export interface Note { // tick 기준으로 함(어짜피 midi 출력은 하지 않을거임)
+export default interface Note { // tick 기준으로 함(어짜피 midi 출력은 하지 않을거임)
     time: number // 노트 배치 위치
     duration: number // 지속시간
     midi: number // 0 ~ 127
