b6c6ed62c2c2ba9b7eb319f6cb77d896afed471d
khyunwook <khyunwook0707@gmail.com>
2024-11-25 11:37:23 +0900

    refactor: 이벤트 버블링 해결 및 버튼 추가

--- src/workspace/parts/channel/Channel.svelte
+++ src/workspace/parts/channel/Channel.svelte
@@ -4,7 +4,12 @@
 
     const dispatcher = createEventDispatcher();
 
-    function deleteChannel() {
+
+
+    // functions
+
+    function deleteChannel(e) {
+        e.stopPropagation();
         dispatcher('delete', { value: channel.trackId})
     }
 
@@ -20,14 +25,27 @@
             }}
         )
     }
+
+    function clicked(e) {
+        e.stopPropagation()
+        console.log("CLICK")
+    }
 </script>
 
 <div class="channel" id={(selected) ? 'selected' : null} onclick={pickChannel} ondblclick={selectChannel}>
     <button onclick={deleteChannel}>X</button>
     {#if channel}
         <div> {channel.trackId} </div>
-        <div> {channel.name.replace('#', iter)} </div>
+        <input type="text" id="name" autocomplete="off"
+               onclick={(e) => e.stopPropagation()}
+               value={channel.name.replace('#', iter)}
+        >
     {/if}
+
+    <div id="buttons">
+        <button id="mute" onclick={clicked}>M</button>
+        <button id="solo" onclick={clicked}>S</button>
+    </div>
 </div>
 
 <style>
@@ -38,11 +56,38 @@
         margin-top: 6px;
         margin-left: calc(10% - 3px);
 
+        display: inline-flex;
+        flex-direction: column;
+
         border: #000000cc solid 3px;
+    }
+
+    .channel > input {
+        background: transparent;
     }
 
     .channel#selected {
 
         border: #00ffffcc solid 3px;
+    }
+
+    #buttons {
+        height: auto;
+
+        display: flex;
+
+        justify-content: space-evenly;
+        align-items: center;
+
+        flex-grow: 1;
+    }
+
+    #buttons > button {
+        width: 40px;
+        height: 30px;
+
+        border: 2px solid black;
+
+        background: #cccccc;
     }
 </style>
